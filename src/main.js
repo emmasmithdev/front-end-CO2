68bff61721073a77ef0553eca0614365e8ad22b4
Emma <emmagray183@gmail.com>
2020-02-03 12:46:03 +0000

    mobile and tablet layouts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import HighchartsVue from 'highcharts-vue'
+import VueMq from 'vue-mq'
 
 Vue.use(HighchartsVue)
 Vue.use(require('vue-moment'));
@@ -12,3 +13,22 @@ export const eventBus = new Vue();
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 450,
+    tablet: 1250,
+    laptop: Infinity,
+  }
+})
+
+new Vue({
+  template: `
+    <grid-component :column="$mq | mq({
+      phone: 2,
+      tablet: 3,
+      laptop: 4
+    })">
+    </grid-component>
+  `,
+})
