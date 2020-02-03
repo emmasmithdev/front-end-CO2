c30bccec4ae0e9bfd87d6b7fb2edf522f19129c5
Emma <emmagray183@gmail.com>
2020-02-03 12:48:54 +0000

    mobile and tablet layouts

--- src/main.js
+++ src/main.js
@@ -24,11 +24,11 @@ Vue.use(VueMq, {
 
 new Vue({
   template: `
-    <footprint :column="$mq | mq({
+    <carbon-form :column="$mq | mq({
       phone: 2,
       tablet: 3,
       laptop: 4
     })">
-    </footprint>
+    </carbon-form>
   `,
 })
