22b236355f7fe8db3887dc4a705a59156d054839
Emma <emmagray183@gmail.com>
2020-02-03 13:22:15 +0000

    mobile and tablet layouts

--- src/components/CarbonForm.vue
+++ src/components/CarbonForm.vue
@@ -178,8 +178,8 @@ export default {
     margin: 5px;
     text-align: center;
     border: solid 2px black;
-    flex-grow: 1;
-    flex-basis: 0;
+    flex: 1 1 0px;
+    width: 0;
   }
   .submit {
     display: block;
