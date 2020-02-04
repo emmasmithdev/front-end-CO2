4be2032a1195d3e49c3240f5f7d9acb3a1de4bf6
Emma <emmagray183@gmail.com>
2020-02-04 15:45:57 +0000

    refactor css button size

--- src/components/CarbonForm.vue
+++ src/components/CarbonForm.vue
@@ -177,8 +177,7 @@ export default {
     margin: 5px;
     text-align: center;
     border: solid 2px black;
-    /* flex: 1 1 0px;
-    width: 0; */
+    flex: 1 1 0px;
   }
   .submit {
     display: block;
